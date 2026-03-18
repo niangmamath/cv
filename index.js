801048431fc00a33ff2c1fd57243a114d9f9c065
mouhamadn63 <mouhamadn63@gmail.com>
2026-03-18 01:06:21 +0000

    Correction du problème de connexion en production

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const Plan = require('./models/plan');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('trust proxy', 1); // Trust the first proxy
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
